47eb9f3a336607cbcb3be6c346ef5da0c66fd727
erab2r <shkera213@gmail.com>
2025-10-25 17:26:35 +0600

    first commit

---
+++ set.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >>n;
+    set<int> s;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin >>x;
+        s.insert(x);
+    // }
+    // auto it = s.begin();
+    // it++;
+    // cout <<*it <<'\n';
+    }
+    for(auto value : s){
+        cout << value <<' ';
+    }
+    cout << '\n';
+    auto it = s.find(25);
+    if(it != s.end()){
+        cout << "Found" <<'\n';
+    }
+    else{
+        cout << "Not Found" <<'\n';
+    }
+    return 0;
+}
